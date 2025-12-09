adcd1641a8d07e95f1ff51b01db71082290c4488
yoyoyo0807 <rerere182152815211@gmail.com>
2025-12-10 01:19:40 +0900

    Update 3_🌐_Systemic_Risk_Map.py

--- pages/3_🌐_Systemic_Risk_Map.py
+++ pages/3_🌐_Systemic_Risk_Map.py
@@ -5,6 +5,6 @@ from utils.data_loader import load_systemic_data
 st.title("🌐 システミックリスク地図")
 
 df_sys = load_systemic_data()
-
 fig = plot_systemic_map(df_sys)
+
 st.plotly_chart(fig, use_container_width=True)
