8b9ab506530b39a88d5830909524b8eeec25265e
yoyoyo0807 <rerere182152815211@gmail.com>
2025-12-10 00:27:54 +0900

    Update qubo_analysis.py

--- utils/qubo_analysis.py
+++ utils/qubo_analysis.py
@@ -1,10 +1,11 @@
+# utils/qubo_analysis.py
 import pandas as pd
+from utils.data_loader import load_hospital_scores
 
-def compute_qubo_vs_rank():
-    df = pd.read_csv("data/systemic_hospital_nodes_compare_qubo_vs_rank_1h_sync.csv")
-    df["overshoot_diff"] = df["overshoot_qubo"] - df["overshoot_rank"]
-    return df
 
-def load_hospital_scores():
-    df = pd.read_csv("data/hospital_systemic_indices_SSS_CDS_SE.csv")
-    return df
+def get_hospital_scores():
+    """
+    Streamlit ページ用のラッパー関数。
+    そのまま load_hospital_scores() を返すだけ。
+    """
+    return load_hospital_scores()
