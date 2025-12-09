0c15374f6cd2dd5ea76f7e7783b2478803b7b814
yoyoyo0807 <rerere182152815211@gmail.com>
2025-12-10 01:20:02 +0900

    Update 5_🏥_Hospital_Risk_Score.py

--- pages/5_🏥_Hospital_Risk_Score.py
+++ pages/5_🏥_Hospital_Risk_Score.py
@@ -1,4 +1,3 @@
-# pages/5_🏥_Hospital_Risk_Score.py
 import streamlit as st
 from utils.data_loader import load_hospital_scores
 
@@ -6,11 +5,7 @@ st.title("🏥 病院別 Systemic リスクスコア")
 
 df_scores = load_hospital_scores()
 
-st.write("hospital_systemic_indices_SSS_CDS_SE.csv から読み込み")
 st.dataframe(
     df_scores.sort_values("SSS", ascending=False),
     use_container_width=True
 )
-
-st.header("Systemic Stress Score")
-st.bar_chart(df_scores.set_index("hospital_name")["SSS_scaled"])
